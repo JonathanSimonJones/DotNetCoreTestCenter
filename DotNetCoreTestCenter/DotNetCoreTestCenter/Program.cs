555b182b11c89a13e8e09f88fc5593412dcd6605
JJ <jonathansimonjones@gmail.com>
2020-11-19 21:09:45 +0000

    Added .NET Core test center project.

---
+++ DotNetCoreTestCenter/DotNetCoreTestCenter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetCoreTestCenter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
